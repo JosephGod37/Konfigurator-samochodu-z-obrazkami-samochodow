88ac8e3dcfc1b30d477b55fcc4c8e803569c5e63
Joseph <Josph@com.pl>
2025-03-14 13:58:39 +0100

    dzialanie strony

--- app/src/main/java/com/example/konfigurator_samochodu_z_obrazkami_samochodw/MainActivity.kt
+++ app/src/main/java/com/example/konfigurator_samochodu_z_obrazkami_samochodw/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.konfigurator_samochodu_z_obrazkami_samochodw
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +21,55 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val myImageView: ImageView = findViewById(R.id.my_ImageView)
+
+        val images = listOf(
+            R.drawable.hatchback,
+            R.drawable.sedan,
+            R.drawable.suv
+        )
+
+        val radioGroup = findViewById<RadioGroup>(R.id.car_radiogroup)
+        var selectedCar = ""
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.hatchback_radiobutton -> {
+                    myImageView.setImageResource(images[0])
+                    selectedCar = "hatchback"
+                }
+
+                R.id.sedan_radiobutton -> {
+                    myImageView.setImageResource(images[1])
+                    selectedCar = "sedan"
+                }
+
+                R.id.suv_radiobutton -> {
+                    myImageView.setImageResource(images[2])
+                    selectedCar= "suv"
+                }
+            }
+        }
+
+        val myCheckBox_klima = findViewById<CheckBox>(R.id.klimatyzacja)
+        var opcja1 = "Nie zaznaczone"
+        var opcja2 = "Nie zaznaczone"
+
+        myCheckBox_klima.setOnCheckedChangeListener { _, isChecked ->
+            opcja1 = if (isChecked) "zaznaczono" else "Brak klimatyzacji"
+        }
+        val myCheckBox_siedzenia = findViewById<CheckBox>(R.id.skorzane_siedzenia)
+        myCheckBox_siedzenia.setOnCheckedChangeListener { _, isChecked ->
+            opcja2= if (isChecked) "skorzane siedzenia" else "Brak skorzanych siedzen"
+        }
+
+        val submitButton = findViewById<Button>(R.id.myButton)
+
+        submitButton.setOnClickListener{
+            val message = "Wybrane auto: $selectedCar\nKlimatyzacja: $opcja1\nSkorzane siedzenia: $opcja2\n"
+            Toast.makeText(this@MainActivity, "Pobrano dane", Toast.LENGTH_LONG).show()
+            Log.d("car",message)
         }
     }
 }
